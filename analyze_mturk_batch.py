a702939570f8aedda537f3604ea2ad33d89dee21
arlittr <arlitt.ryan@gmail.com>
2017-06-21 15:57:35 +0800

    Cleanup and Control Statements
    -Generalized makeinputs
    -Weave in one control statement per HIT
    -Bug fixes
    -cleanup in analyze_mturk_batch

--- analyze_mturk_batch.py
+++ analyze_mturk_batch.py
@@ -19,49 +19,11 @@ def setQuestionList(k):
     return list(key_df[key_df['k']==k]['idea'])
 
 def setBatchCorrect(answer,ideas):
-    
-    
+    #For each question, reverse lookup correctness of answer in keyfile
     ideas = list(ideas)
-#    print(ideas)
-    
-    
-#    ideas = pd.DataFrame(ideas).transpose()
-#    ideas = ideas.transpose()
-#    print(key_df(list(ideas.index)))
-#    print(ideas.index)
-#    
-#    print("answer: ",answer)
-#    print("ideas: ",ideas,type(ideas))
-##    ideas.sort()
-#
-##    print(''.join(ideas))
-#    ideas_joined = ideas.apply(lambda x: ''.join(x))
-##    ideas_joined = ''.join(sorted(ideas))
-#
-#    print('sorted ideas: ',ideas_joined)
-##    print("ideas:",ideas)
-#
-###    print("answer:",answer)
     selected_response = ideas[answer]
-##    print(key_df[(key_df['idea']==selected_response) & (key_df['question_list_joined']==ideas_joined)])
-##    is_correct = key_df[(key_df['idea']==selected_response) & (key_df['question_list_joined']==ideas_joined)]['correct']
-##    print(is_correct,type(is_correct))
-##    return is_correct
-###    is_correct[0]
-##    
-#    return key_df[(key_df['idea']==selected_response) & (key_df['question_list_joined']==ideas_joined)]['correct']
-##    print(selected_response)
-#    print(key_df[key_df['idea']==selected_response])
-#    print(key_df[key_df['question_list_str'] == str(ideas)])
-#    print(key_df['idea']==selected_response)
-#    print(key_df['question_list_str'] == str(ideas))
-#    print((key_df['idea']==selected_response) & (key_df['question_list_str'] == str(ideas)))
     is_correct = key_df[(key_df['idea']==selected_response) & (key_df['question_list_str'] == str(ideas))]['correct'].iloc[0]
     return is_correct
-##    print(key_df[key_df['idea']==selected_response]['correct'])
-##    
-##    return key_df[key_df['idea']==selected_response]['correct']
-    
     
     
 if __name__ == '__main__':
@@ -79,30 +41,20 @@ if __name__ == '__main__':
     keyfile_path = inputbasepath + keyfile_basename + fileextension
     key_df = pd.read_csv(keyfile_path)
     
-    
-    input_answer_mapping = {'Input.idea1':'Answer.Q1Answer',
-                            'Input.idea2':'Answer.Q2Answer',
-                            'Input.idea3':'Answer.Q3Answer'}
-    
     #mapping between answer field and selectable answers
     #in batch results, values for answers must be in sequential order (eg idea1a = 1, idea1b = 2) 
     answer_input_mapping = {'Answer.Q1Answer':['Input.idea1','Input.idea1a','Input.idea1b'],
                             'Answer.Q2Answer':['Input.idea2','Input.idea2a','Input.idea2b'],
                             'Answer.Q3Answer':['Input.idea3','Input.idea3a','Input.idea3b']
                             }
-
-#    key_df['correct'] = key_df.apply(lambda row: setCorrect(row['k'],row['cluster']),axis=1)
-#    
-#    key_df['question_list'] = key_df.apply(lambda row: setQuestionList(row['k']),axis=1) 
-    
-    
+ 
     #prepare question list as lists rather than strings
     key_df['question_list'] = key_df['question_list'].apply(lambda x: literal_eval(x))
     key_df['question_list_str'] =  key_df['question_list'].apply(lambda x: str(x))
 
     for k,v in answer_input_mapping.items():
-        print(k,v)
         batch_df[k+'_correct'] = batch_df.apply(lambda row: setBatchCorrect(row[k],row[v]),axis=1)
+        
      #if list(row[v]) == list(question_df['idea'])
         
 #    for k,v in answer_input_mapping.items():
--- mturk_makeinputs.py
+++ mturk_makeinputs.py
@@ -7,6 +7,8 @@ Created on Wed Jun 14 14:09:45 2017
 """
 
 import pandas as pd
+import random
+import csv
 
 def setCorrect(k,cluster,this_df):
     if list(this_df[this_df['k']==k]['cluster']).count(cluster) >= 2:
@@ -16,6 +18,10 @@ def setCorrect(k,cluster,this_df):
 
 def setQuestionList(k,this_df):
     return list(this_df[this_df['k']==k]['idea'])
+
+def getControlStatement(control_df):
+    return control_df.sample(n=1)
+    
 
 if __name__ == '__main__':
     inputbasepath = '/Volumes/SanDisk/Repos/distributed_ideation/input_data/topic_clustering_results/'
@@ -27,45 +33,85 @@ if __name__ == '__main__':
     
     df = pd.read_csv(path)
     
-    #TODO: Weave in one(?) control question per HIT
+    #Weave in N control questions per HIT (prob max 4 total Qs per HIT)
+    control_questions_per_HIT = 1
     
+    #Set min desired number of unique questions in keyfile
+    total_questions = 1000
+    
+    #%%
     #make keyfile
     kf = pd.DataFrame(columns=df.columns)
+    
+    #restrict cluster size
     cluster_min_size = 5
     cluster_max_size = 50
-
     cluster_ids = set(df[(df['n'] >= cluster_min_size) & (df['n'] <= cluster_max_size)]['cluster'])
-        
+    
+    
+    #stratified sample of selected clusters
     k=0
-    for cluster_id in cluster_ids:
-        kftemp = df[df['cluster']==cluster_id].sample(n=2)
-        kftemp = kftemp.append(df[(df['cluster']!=cluster_id) & (df['cluster'].isin(cluster_ids))].sample(n=1))
-        kftemp['k'] = k
-        kf = kf.append(kftemp)
-        k+=1
+    while k < total_questions:
+        for cluster_id in cluster_ids:
+            kftemp = df[df['cluster']==cluster_id].sample(n=2)
+            kftemp = kftemp.append(df[(df['cluster']!=cluster_id) & (df['cluster'].isin(cluster_ids))].sample(n=1))
+            kftemp['k'] = k
+            kf = kf.append(kftemp)
+            k+=1 
     
     #Added improved keyfile later
 #    outpath = outputbasepath + basename + '_mturk_keyfile' + '.csv'
 #    kf.to_csv(outpath,encoding='utf-8')
        
-    
     #make inputfile
     #Use headers to control how many questions per HIT
-    headers = ['idea1','idea1a','idea1b','idea2','idea2a','idea2b','idea3','idea3a','idea3b']
+    headers = ['idea1','idea1a','idea1b','idea2','idea2a','idea2b','idea3','idea3a','idea3b','idea4','idea4a','idea4b']
+    questions_per_HIT = int(len(headers) / 3)
+    items_per_question = 3
     #Use max_HITs to control how many HITs to generate
-    max_HITs = 2
+    max_HITs = 100
     mtin = pd.DataFrame()
     for k in set(kf['k']):
+        #grab the first idea as the baseline
         mtintemp = kf[kf['k']==k].iloc[[0]]
         #randomize the order of the other ideas
-        mtintemp = mtintemp.append(kf[kf['k']==k].sample(n=2,weights=[0,1,1]))
+        mtintemp = mtintemp.append(kf[kf['k']==k].sample(n=items_per_question-1,weights=[0]+(items_per_question-1)*[1]))
         mtin = mtin.append(mtintemp)
+    
+    #add correctness bool to each item for improved keyfile
+    mtin['correct'] = mtin.apply(lambda row: setCorrect(row['k'],row['cluster'],mtin),axis=1)
+
+    #randomly shuffle around each question triad
+    shuffled_inds = random.sample(range(int(max(mtin['k'])+1)),int(max(mtin['k'])+1))
+    mtin['shuffled_ks'] = [item+i for item in shuffled_inds for i in [0.0,0.1,0.2]]
+    mtin.sort_values('shuffled_ks',inplace=True)
+    mtin['k_new'] = mtin['shuffled_ks'].apply(lambda x: int(x))
+
+    #%%
+    #weave in control text
+    control_path = '/Volumes/SanDisk/Repos/distributed_ideation/results/mturk/moby_dick.csv'
+    with open(control_path,'r') as controlTextFile:
+        reader = csv.reader(controlTextFile, dialect='excel')
+        control_text = [r for r in reader][0]
+    
+    mtin['is_control'] = False
+    mtin = mtin.reset_index()    
+    seedvec = range(len(mtin[mtin['correct']==False]))[0::questions_per_HIT]
+    replace_inds=[]
+    false_answer_indices = list(mtin[mtin['correct']==False].index)
+    for low,high in zip(seedvec,[s+(questions_per_HIT-1) for s in seedvec]):
+        low = low + 1 #skip first question for weaving control question
+        replace_ind = random.sample(false_answer_indices[low:high+1],control_questions_per_HIT)
+        replace_inds.append(replace_ind)
+        mtin.set_value(replace_ind,'idea',random.sample(control_text,1))
+        mtin.set_value(replace_ind,'is_control',True)
+    
+    #%%
     
     #add question order to each item for improved keyfile
     mtin['question_list'] = mtin.apply(lambda row: setQuestionList(row['k'],mtin),axis=1)
-    #add correct bool to each item for improved keyfile
-    mtin['correct'] = mtin.apply(lambda row: setCorrect(row['k'],row['cluster'],mtin),axis=1)
     
+    #%%
     #Improved keyfile with addition columns
     outpath = outputbasepath + basename + '_mturk_keyfile' + '.csv'
     mtin.to_csv(outpath,encoding='utf-8')
